d89230fe55a7e9ebc1d111dd07ba6ddee7ac5701
xuyue7 <1873130966@qq.com>
2018-10-19 16:06:19 +0800

    第一次提交

---
+++ src/main/java/com/utils/SpringException.java
@@ -0,0 +1,14 @@
+package com.utils;
+
+public class SpringException extends RuntimeException {
+	
+	private String exceptionMsg;
+
+	public SpringException(String exceptionMsg) {
+		this.exceptionMsg = exceptionMsg;
+	}
+
+	public String getExceptionMsg() {
+		return this.exceptionMsg;
+	}
+}
